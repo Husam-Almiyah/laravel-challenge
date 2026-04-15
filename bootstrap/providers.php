ca266effb690e1e0c04addfbc507762e9e0ff31c
HusamAl-Miyah <husamalmiyah@gmail.com>
2026-04-15 23:39:52 +0300

    build: standardize development environment and core dependencies

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Domains\Payments\Providers\PaymentServiceProvider;
 use App\Providers\AppServiceProvider;
 
 return [
     AppServiceProvider::class,
+    PaymentServiceProvider::class,
 ];
